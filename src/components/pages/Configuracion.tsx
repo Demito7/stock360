0884a37ace6705945033625f0752e0cbaeeffe8d
Demito7 <demianaraujo7@gmail.com>
2025-05-22 16:36:29 -0300

    fix: corregido vite.config para build exitoso

---
+++ src/components/pages/Configuracion.tsx
@@ -0,0 +1,14 @@
+import "./pages.css";
+import Navegacion from "../Navegacion";
+
+function Configuraciones() {
+    return (
+        <>
+            <div className="navegacion">
+                <h2>Configuracion</h2>
+                <Navegacion />
+            </div>
+        </>
+    )
+}
+export default Configuraciones
---
+++ src/components/pages/Deudor.tsx
@@ -0,0 +1,14 @@
+import "./pages.css";
+import Navegacion from "../Navegacion";
+
+function Deudores() {
+    return (
+        <>
+            <div className="navegacion">
+                <h2>Deudor</h2>
+                <Navegacion />
+            </div>
+        </>
+    )
+}
+export default Deudores

33ed30361796f67f455697130f89bb7853d06572
Demito7 <demianaraujo7@gmail.com>
2025-05-24 19:11:47 -0300

    Cambio de puerto para Render y ajustes finales

--- server/index.js
+++ server/index.js
@@ -130,5 +130,8 @@ app.put("/articulo/:id", async (req, res) => {
 });
 
 
-app.listen(5000, () => {
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
